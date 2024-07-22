3e41bbe1c6961372b19c24e409e7da86f361aab3
Dang Minh <minhdangngoc304@gmail.com>
2024-07-22 22:10:54 +0700

    Search Screen

--- app/src/main/java/com/example/newsapp/presentation/nvgraph/NavGraph.kt
+++ app/src/main/java/com/example/newsapp/presentation/nvgraph/NavGraph.kt
@@ -13,6 +13,8 @@ import com.example.newsapp.presentation.home.HomeScreen
 import com.example.newsapp.presentation.home.HomeViewModel
 import com.example.newsapp.presentation.onboarding.OnBoardingScreen
 import com.example.newsapp.presentation.onboarding.OnBoardingViewModel
+import com.example.newsapp.presentation.search.SearchScreen
+import com.example.newsapp.presentation.search.SearchViewModel
 
 @Composable
 fun NavGraph(
@@ -41,9 +43,8 @@ fun NavGraph(
             startDestination = Route.NewsNavigatorScreen.route
         ){
             composable(route = Route.NewsNavigatorScreen.route){
-                val viewModel: HomeViewModel = hiltViewModel()
-                val articles = viewModel.news.collectAsLazyPagingItems()
-                HomeScreen(articles = articles, navigate = {})
+                val viewModel: SearchViewModel = hiltViewModel()
+                SearchScreen(state = viewModel.state.value, event = viewModel::onEvent, navigate = {})
             }
         }
     }
